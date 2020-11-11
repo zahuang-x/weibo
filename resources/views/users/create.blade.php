9892b7d898d9727f7e5cba106c20b8152af89408
zahuang-x <x.zahuang@gmail.com>
2020-11-11 13:25:52 +0900

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.default')
-@section('title', '注册')
+@section('title', '登録')
 
 @section('content')
-<h1>注册</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>登録</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+          <div class="form-group">
+            <label for="name">名前：</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">メール：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">パスワード：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">パスワード確認：</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">登録</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
