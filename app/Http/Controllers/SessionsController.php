deead9af2d89b4ac05f50aefcaf197cdabd3ef44
zahuang-x <x.zahuang@gmail.com>
2020-11-11 16:56:35 +0900

    记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -17,7 +17,7 @@ class SessionsController extends Controller
             'email' =>  'required|email|max:255',
             'password' =>   'required'
         ]);
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('rememberme'))){
             session()->flash('success','お帰りなさい');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
@@ -28,7 +28,7 @@ class SessionsController extends Controller
 
     public function destroy(){
         Auth::logout();
-        session()->flash('success','ログインしてください');
+        session()->flash('success','ログアウトしました');
         return redirect('login');
     }
 }
